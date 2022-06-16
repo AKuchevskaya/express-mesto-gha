2b7030b2f58e122c4559e9d786bce3a504890458
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2022-06-16 10:19:22 +0300

    feat: создать схему и модель карточки

---
+++ models/card.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+//описываем модель
+const cardSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+        minlength: 2,
+        maxlength: 30
+    },
+    link: {
+        type: String,
+        require: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        require: true
+    },
+    likes: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: []
+    },
+    createdAt: {
+        type: Data,
+        default: Data.now
+    }
+});
+
+//создаем модель и экспортируем ее
+module.exports = mongoose.model('card', cardSchema)
