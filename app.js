fabd2097780ca7eeba4de7b04f3eb97c663f8370
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2022-06-16 09:53:42 +0300

    feat: создать схему и модель пользователя

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const {PORT = 3000} = process.env;
 
 const app = express();
+
 //подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
+
 //подключаем мидлвары, роуты и тд
 
 app.use(express.static(path.resolve(__dirname, 'build')))
---
+++ models/user.js
@@ -0,0 +1,25 @@
+const { urlencoded } = require('body-parser');
+const mongoose = require('mongoose');
+
+//описываем модель
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+        minlength: 2,
+        maxlength: 30
+    },
+    about: {
+        type: String,
+        require: true,
+        minlength: 2,
+        maxlength: 30
+    },
+    avatar: {
+        type: String,
+        require: true
+    }
+});
+
+//создаем модель и экспортируем ее
+module.exports = mongoose.model('user', userSchema)
